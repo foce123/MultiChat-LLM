16571b88250275863779eb3fd3f39dda006814d6
foce123 <foce123@sina.com>
2023-06-28 09:06:54 +0800

    update startup

--- config.py
+++ config.py
@@ -16,23 +16,36 @@ def read_file(path) -> dict:
         logger.error("[ERROR] configs file: {} not exist!!!".format(path))
 
 
-def load_config():
+def load_config(conf_name):
     global chat_conf, bot_conf
     configPath = "config.json"
     app_configPath = "./configs/app-config.json"
     bot_configPath = "./configs/bot-config.json"
     agent_configPath = "/configs/agent-config.json"
-    config = read_file(configPath)
-    app_conf = read_file(app_configPath)
-    print(type(chat_conf))
-    print(chat_conf)
-    if config['chat'] == "wechat":
-        chat_conf = app_conf["wechat"]
-    elif config['chat'] == "dingtalk":
-        chat_conf = app_conf["dingtalk"]
+    if conf_name == "config":
+        config = read_file(configPath)
+        return config
+    elif conf_name == "app_config":
+        config = read_file(app_configPath)
+        return config
+    elif conf_name == "bot_config":
+        config = read_file(bot_configPath)
+        return config
+    elif conf_name == "agent_config":
+        config = read_file(agent_configPath)
+        return config
+    else:
+        logger.error("[ERROR] config file not exist!!!")
+        os.error("config file not exist!!!")
 
-    if config["chatbot"]:
-        bot_conf = read_file(bot_configPath)
+    # app_conf = read_file(app_configPath)
+    #
+    # if config['chat'] == "wechat":
+    #     chat_conf = app_conf["wechat"]
+    # elif config['chat'] == "dingtalk":
+    #     chat_conf = app_conf["dingtalk"]
+    #
+    # if config["chatbot"]:
+    #     bot_conf = read_file(bot_configPath)
 
 
-load_config()
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 
-from config import *
+from config import load_config
 
 if __name__ == '__main__':
-    if chat_conf.name == "wechat":
-        pass
-    elif chat_conf.name == "dingtalk":
-        print("dingtalk")
+    conf = load_config("config")
+    print(conf['chat'])
+    # if chat_conf.name == "wechat":
+    #     pass
+    # elif chat_conf.name == "dingtalk":
+    #     print("dingtalk")
         # from app.dingtalk import application
         # application.run()
