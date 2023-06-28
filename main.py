73d127a33c5939201db596bfe9cc97b6b434b919
foce123 <foce123@sina.com>
2023-06-28 09:52:49 +0800

    update startup

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ from config import load_config
 
 if __name__ == '__main__':
     conf = load_config("config")
-    print(conf['chat'])
-    # if chat_conf.name == "wechat":
-    #     pass
-    # elif chat_conf.name == "dingtalk":
-    #     print("dingtalk")
+    if conf['chat'] == "wechat":
+        pass
+    elif conf['chat'] == "dingtalk":
+        print("dingtalk")
         # from app.dingtalk import application
         # application.run()
