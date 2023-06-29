71d79b7dcd3881327b296c7bb300ca6320095983
foce123 <foce123@sina.com>
2023-06-29 16:26:05 +0800

    update startup

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ if __name__ == '__main__':
     conf = load_config("config")
     # support "wechat" and "dingtalk" chat type.
     if conf['chat'] == "wechat":
+        from app.wechat.wechat_channel import WechatChannel
+        WechatChannel.startup()
         print("load wechat")
     elif conf['chat'] == "dingtalk":
         print("load dingtalk")
